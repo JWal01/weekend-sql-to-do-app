295a0c9a8fc9f970cffe5c2c59e04ff5825f8848
Jeff Walter <Jeff.Walter@me.com>
2023-08-13 21:28:06 -0500

    added code so the get route is running

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -5,7 +5,7 @@ const pool = require('../modules/pool.js');
 // GET
 router.get('/', (req, res) => {
  
-  const sqlText = `SELECT * FROM tasks ORDER BY name, origin DESC;`;
+  const sqlText = `SELECT * FROM tasks ORDER BY id;`;
   pool.query(sqlText)
       .then((result) => {
           console.log(`Got stuff back from the database`, result);
@@ -19,11 +19,11 @@ router.get('/', (req, res) => {
 
 // POST
 router.post('/', (req, res) => {
-  const tasks = req.body;
-  const sqlText = `INSERT INTO tasks (task, complete)
-                   VALUES ($1, $2)`;
+  let newTask = req.body;
+  let sqlText = `INSERT INTO tasks (task, complete)
+  VALUES ($1,$2)`
 
-  pool.query(sqlText, [tasks.task, tasks.complete])
+  pool.query(sqlText, [newTask.task, newTask.complete])
       .then((result) => {
           console.log(`Added tasks to the database`, tasks);
           res.sendStatus(201);
@@ -36,7 +36,30 @@ router.post('/', (req, res) => {
 
 
 // PUT
-
+// router.put('/:id', (req,res) =>{
+//     let id =  req.params.id 
+//     let queryText = `UPDATE "tasks" SET "rank" = +1 WHERE "id" = $1;`;
+//     pool.query(queryText, [id])
+//     .then((result) => {
+//         res.sendStatus(201);
+//     })
+//     .catch((err) => {
+//         console.log(`Error making query ${queryText}`, err);
+//         res.sendStatus(500)
+//     })
+// })
 // DELETE
+// router.delete('/:id', (req,res) => {
+//     let id = req.params.id;
+//     let queryText = `DELETE FROM "tasks" WHERE "id" = $1;`;
+//     pool.query(queryText, [id])
+//     .then((result) => {
+//         res.sendStatus(200);
+//     })
+//     .catch((err) => {
+//         console.log(`Error making query ${queryText}`, err);
+//         res.sendStatus(500)
+//     })
+// })
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,17 +4,18 @@ import './App.css';
 
 function App () {
 
-  const[taskList , setTaskList] = useState('');
+  
   
   return (
     <div>
       <h1>TO DO APP</h1>
-      <form>
-        <input className='input' placeholder='Add Task'/>
+      <form onSubmit={''}>
+        <input  className='input' placeholder='Add Task'/>
         <button className='addButton' type="submit">Add</button>
 
       </form>
 
+     
     </div>
   );
 
